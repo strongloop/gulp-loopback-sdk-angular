78ec7df44c6a571929efad68d634b9d7c8ff791f
Zi Ming Lin <zi.lin9@gmail.com>
2014-09-09 09:44:29 -0400

    Update to LoopBack 2.0
    Updated package.json to refer to latest versions of dependencies.
    Also cleaned up tabs in some of the files.
    
    Closes #4

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var gutil = require('gulp-util');
 var through = require('through2');
 var generator = require('loopback-sdk-angular');
+var path = require('path');
 
 module.exports = function (options) {
   return through.obj(function (file, enc, cb) {
@@ -13,16 +14,16 @@ module.exports = function (options) {
 
     var app;
     try {
-      app = require(file.path);
+      app = require(path.resolve(file.path));
 
       //  Incase options is undefined.
-      options = options || { ngModuleName: 'lbServices', apiUrl: undefined };
-      
+      options = options || {ngModuleName: 'lbServices', apiUrl: undefined};
+
       options.ngModuleName = options.ngModuleName || 'lbServices';
       options.apiUrl = options.apiUrl || app.get('restApiRoot') || '/api';
 
       gutil.log('Loaded LoopBack app', gutil.colors.magenta(file.path));
-			gutil.log('Generating',
+      gutil.log('Generating',
         gutil.colors.magenta(options.ngModuleName),
         'for the API endpoint',
         gutil.colors.magenta(options.apiUrl)
@@ -35,14 +36,15 @@ module.exports = function (options) {
       );
 
       file.contents = new Buffer(script);
-      
-      gutil.log('Generated Angular services file.'); 
 
-			this.push(file);
-		} catch (err) {
-			this.emit('error', new gutil.PluginError('gulp-loopback-sdk-angular', err));
-		}
+      gutil.log('Generated Angular services file.');
 
-		cb();
-	});
+      this.push(file);
+    } catch (err) {
+      this.emit('error', new gutil.PluginError('gulp-loopback-sdk-angular', err));
+    }
+
+    cb();
+    return;
+  });
 };
--- test.js
+++ test.js
@@ -5,25 +5,25 @@ var loopbackAngular = require('./index');
 var fs = require('fs');
 
 it('should ', function (cb) {
-	var stream = loopbackAngular();
+  var stream = loopbackAngular();
 
-	stream.on('data', function (file) {
-		assert.equal(
+  stream.on('data', function (file) {
+    assert.equal(
       file.contents.length,
       fs.statSync('fixtures/lb-services.js').size
     );
     assert(/var urlBase = "\/rest-api-root";/.test(file.contents));
     assert(/var module = angular.module\("lbServices", \['ngResource'\]\);/
       .test(file.contents));
-	});
+  });
 
-	stream.on('end', cb);
+  stream.on('end', cb);
 
-	stream.write(new gutil.File({
+  stream.write(new gutil.File({
     base: __dirname,
-		path: __dirname + '/fixtures/app.js',
+    path: __dirname + '/fixtures/app.js',
     contents: fs.readFileSync('fixtures/app.js')
-	}));
+  }));
 
-	stream.end();
+  stream.end();
 });
