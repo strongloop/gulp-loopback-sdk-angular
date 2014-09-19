3309fe331395fc4a13c92275db7a1407f0fc3489
Zi Ming Lin <zi.lin9@gmail.com>
2014-09-19 16:04:38 -0400

    Prevent gulp from hanging when loopback connects to a database
    I used Watch.JS to call disconnect on a datasource when the datasource
    is done connecting. This is not guaranteed to work on all connectors at
    the moment.
    
    References #3

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ var gutil = require('gulp-util');
 var through = require('through2');
 var generator = require('loopback-sdk-angular');
 var path = require('path');
+var WatchJS = require('watchjs');
+
+var watch = WatchJS.watch;
+var unwatch = WatchJS.unwatch;
+
+//  This callback will be passed to watch, this refers to the datasource
+function disconnectDataSource(prop, action, newValue) {
+  if (newValue === true) {
+    this.disconnect();
+    unwatch(this, 'connected');
+  }
+}
 
 module.exports = function (options) {
   return through.obj(function (file, enc, cb) {
@@ -40,6 +52,14 @@ module.exports = function (options) {
       gutil.log('Generated Angular services file.');
 
       this.push(file);
+
+      var dataSources = app.dataSources;
+      for (var dataSource in dataSources) {
+        if (dataSources.hasOwnProperty(dataSource)) {
+          var ds = dataSources[dataSource];
+          watch(ds, 'connected', disconnectDataSource);
+        }
+      }
     } catch (err) {
       this.emit('error', new gutil.PluginError('gulp-loopback-sdk-angular', err));
     }
